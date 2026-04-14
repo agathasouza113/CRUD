2102695f27e091c91410046ec17a4ab6edb16adf
Ágatha de Souza <agathasouzasenai113@gmail.com>
2026-04-14 11:35:22 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD Cadastro de Usuário</h1>
+      <label>Nome</label>
+      <input type="text" id="nome"/><br/>
+
+      <label>Sobrenome</label>
+      <input type="text" id="sobrenome"/><br/>
+ 
+      <label>E-mail</label>
+      <input type="text" id="email"/><br/>
+
+      <label>Telefone</label>
+      <input type="number" id="telefone"/><br/>
+
+
+      <label>CPF</label>
+      <input type="number" id="cpf"/><br/>
+
+      <button type="button" onclick="cadasrtrarUsuario()">Cadastre-se</button>
+      <p id="msgcadastro"></p>
+
+
+      <button type="button" onclick="editarUsuario()">Editar</button>
+      <p id="msgeditar"></p>
+
+      <button type="button" onclick="excluirUsuario()">Excluir</button>
+      <p id="msgexcluir"></p>
     </div>
   );
-}
+};
 
 export default App;
